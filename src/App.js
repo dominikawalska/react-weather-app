f8ece73da36b53d15f9116d482269df32a1a913c
dominikawalska <dominikawalska@gmail.com>
2022-05-05 22:49:29 +0200

    Added "Search" and "MainWindow" components with styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Search from "./Search";
+import MainWindow from "./MainWindow";
+import Container from "react-bootstrap/Container";
 
 export default function App() {
   return (
-    <div className="App">
-      Weather
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://github.com/dominikawalska"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Dominika Walska
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/dominikawalska/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
-    </div>
+    <Container>
+      <div className="weather-app">
+        <div className="weather-frame">
+          <Search />
+          <MainWindow />
+          <div>
+            <a
+              href="https://github.com/dominikawalska/weather-react-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-sourced code
+            </a>{" "}
+            by{" "}
+            <a
+              href="https://github.com/dominikawalska"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Dominika Walska
+            </a>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 }
