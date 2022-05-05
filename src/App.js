b4e8e11c9cad0aae3452d793a8ae51d26a4ecdfc
dominikawalska <dominikawalska@gmail.com>
2022-05-05 21:08:08 +0200

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather</div>;
+  return (
+    <div className="App">
+      Weather
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://github.com/dominikawalska"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dominika Walska
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/dominikawalska/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
